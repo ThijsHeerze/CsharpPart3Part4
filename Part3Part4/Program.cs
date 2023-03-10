33046386f753452be2fece3b01108b9058aa62e5
Thijs Heerze <thijsheerze@gmail.com>
2023-03-10 17:43:50 +0100

    first commit

---
+++ Part3Part4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part3Part4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
