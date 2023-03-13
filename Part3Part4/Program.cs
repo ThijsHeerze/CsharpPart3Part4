2cd65d055a5acc864e76db765644291a3d10b887
Thijs Heerze <thijsheerze@gmail.com>
2023-03-13 12:13:43 +0100

    exercise 3-24

--- Part3Part4/Program.cs
+++ Part3Part4/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Login();
+
+            static void Login()
+            {
+                string username = "alex";
+                string username2 = "emma";
+                string password = "sunshine";
+                string password2 = "haskell";
+
+                Console.WriteLine("Enter username");
+                string input1 = Console.ReadLine();
+                Console.WriteLine("Enter password");
+                string input2 = Console.ReadLine();
+
+                if (username == input1 && password == input2 || username2 == input1 && password2 == input2)
+                {
+                    Console.WriteLine("You have successfully logged in!");
+                } else
+                {
+                    Console.WriteLine("Incorrect username or password!");
+                }
+            }
         }
     }
 }
