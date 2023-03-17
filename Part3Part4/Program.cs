87819f25a388e87cb97aabb3a74549cba0166d39
Thijs Heerze <thijsheerze@gmail.com>
2023-03-17 12:54:15 +0100

    exercise 3-31

--- Part3Part4/Program.cs
+++ Part3Part4/Program.cs
@@ -4,13 +4,15 @@
     {
         static void Main(string[] args)
         {
-            //exercise 3-24
+
 
             //Login();
             //LastPartSplit();
             //CSVAge();
-            CSVName();
+            //CSVName();
+            MaxNameAndAge();
 
+            //exercise 3-24
             static void Login()
             {
                 string username = "alex";
@@ -66,6 +68,7 @@
                 Console.WriteLine($"Age of the oldest: {maxAge}.");
             }
 
+            //exercise 3-30
             static void CSVName()
             {
                 string input;
@@ -78,7 +81,7 @@
                 {
 
                     string[] parts = input.Split(',');
-                    string name = parts[0].Trim();
+                    string name = parts[0];
 
                     int age = int.Parse(parts[1]);
                     if (age > maxAge)
@@ -88,6 +91,51 @@
                     }
                 }
                 Console.WriteLine($"The name of the oldest: {maxAgeName}.");
+            }
+
+            //exercise 3-31
+            static void MaxNameAndAge()
+            {
+                List<string> names = new List<string>();
+                List<int> birthYears = new List<int>();
+
+                while (true)
+                {
+                    Console.Write("Enter name and birth year (or press Enter to finish): ");
+                    string input = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        break;
+                    }
+
+                    string[] parts = input.Split(',');
+                    string name = parts[0].Trim();
+                    int birthYear = int.Parse(parts[1]);
+                    names.Add(name);
+                    birthYears.Add(birthYear);
+                }
+
+                int currentYear = DateTime.Now.Year;
+                int maxAge = int.MinValue;
+                string longestName = string.Empty;
+
+                for (int i = 0; i < names.Count; i++)
+                {
+                    string name = names[i];
+                    int birthYear = birthYears[i];
+                    int age = currentYear - birthYear;
+
+                    if (age > maxAge || (age == maxAge && name.Length > longestName.Length))
+                    {
+                        maxAge = age;
+                        longestName = name;
+                    }
+                }
+
+                Console.WriteLine("The longest name is: " + longestName);
+                Console.WriteLine("The highest age is: " + maxAge);
+                Console.ReadLine();
             }
         }
     }
