9314714b257388b07d2469798c767a9677e79bf6
Thijs Heerze <thijsheerze@gmail.com>
2023-03-20 12:43:51 +0100

    exercise 4-22

--- Part3Part4/Program.cs
+++ Part3Part4/Program.cs
@@ -1,65 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Reflection;
 
-class Book
-{
-    public string Title { get; set; }
-    public int Pages { get; set; }
-    public int PublicationYear { get; set; }
-}
 
-class Program
+class MainClass
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        List<Book> books = new List<Book>();
+        int count = 0;
+        string input = "";
 
-        // Read book information from user
-        while (true)
+        while (input != "end")
         {
-            Console.Write("Name: ");
-            string title = Console.ReadLine();
-
-            if (string.IsNullOrEmpty(title))
+            input = Console.ReadLine();
+            if (input != "end")
             {
-                break;
-            }
-
-            Console.Write("Pages: ");
-            int pages = int.Parse(Console.ReadLine());
-
-            Console.Write("Publication year: ");
-            int publicationYear = int.Parse(Console.ReadLine());
-
-            Book book = new Book
-            {
-                Title = title,
-                Pages = pages,
-                PublicationYear = publicationYear
-            };
-
-            books.Add(book);
-        }
-
-        // Ask user what to print
-        Console.Write("What information will be printed? ");
-        string input = Console.ReadLine();
-
-        if (input == "everything")
-        {
-            // Print all book details
-            foreach (Book book in books)
-            {
-                Console.WriteLine("{0}, {1} pages, {2}", book.Title, book.Pages, book.PublicationYear);
+                count++;
             }
         }
-        else if (input == "title")
-        {
-            // Print only book titles
-            foreach (Book book in books)
-            {
-                Console.WriteLine(book.Title);
-            }
-        }
+
+        Console.WriteLine(count);
     }
 }
